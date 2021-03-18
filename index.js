53f2db818e6c7aef10dcf761856b2f9f7abca1be
wgyt <github@wgyt.tk>
2021-03-18 13:31:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ module.exports = function (host, res, req, error, version, ejs, config) {
    var dirname = config.pagedir  // config file
    var page404 = config["404"]  //  see setup.md
    doesitexist = fs.pathExists(`${dirname + res.url}.html`, (err, exists) => {console.log(err);return exists;})
-   if (doesitexist){
-       res.writeHead(200)
-       res.write(fs.readFileSync(`${dirname + res.url}.html`, "utf8"))
+   if (res.url.startsWith('/static/')){
+       if(doesitexist){
+           res.writeHead(200)
+           res.write(fs.readFileSync(`${dirname + res.url}`, "utf8"))
+       }else{
+           res.writeHead(404)
+           res.write('404')
+       }
    }else{
-    res.writeHead(404)
-    res.write(fs.readFileSync(`${dirname + page404}.html`), "utf8")
+       if (doesitexist){
+           res.writeHead(200)
+           res.write(fs.readFileSync(`${dirname + res.url}.html`, "utf8"))
+       }else{
+        res.writeHead(404)
+        res.write(fs.readFileSync(`${dirname + page404}.html`), "utf8")
+       }    
    }
    res.end()
 }
